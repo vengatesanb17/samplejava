191213c8a0a0a07f956d8fca1a06e8ee16ab6f54
Vengatesan B <Vengatesan B@010223HP840G5100>
2023-04-05 10:52:54 +0530

    first commit

---
+++ simplejava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Vengatesan B
+ *
+ */
+module simplejava {
+}
---
+++ simplejava/src/simplejava/demo.java
@@ -0,0 +1,10 @@
+package simplejava;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello world --- vengatesan balakrishnan");
+
+	}
+
+}
